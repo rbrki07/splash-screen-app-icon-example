2956781678bb46de5ddc71c794a71658da83f80a
René Wilby <rene.wilby@timebook.com>
2021-10-25 13:34:48 +0200

    add splash screen loading example

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// @ts-check
+import React, { useCallback, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
 
-export default function App() {
+const App = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    const initApp = async () => {
+      // Keep the splash screen visible while initialising app
+      await SplashScreen.preventAutoHideAsync();
+      // Example for time consuming initialisation
+      await new Promise((resolve) => setTimeout(resolve, 2500));
+      setAppIsReady(true);
+    };
+    initApp();
+  }, []);
+
+  const onLayoutCallback = useCallback(async () => {
+    if (appIsReady) {
+      // Hide splash screen once initialisation is done
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View onLayout={onLayoutCallback} style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default App;
